0c03e4af8dd3c8489c16a6e1fa6092b72108f7ca
maximilianopontin <maxipontin92@gmail.com>
2025-02-03 22:09:48 -0300

    w

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,19 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
-import { ValidationPipe } from "@nestjs/common"
 import * as cors from "cors"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  app.useGlobalPipes(new ValidationPipe()) // Moved this line up
-
   // Configurar CORS
   app.use(
     cors({
-      origin: ["https://api-react-movie.onrender.com", "http://localhost:5173"],
+      origin: ["http://localhost:5173", "https://api-react-movie.onrender.com/"],
       methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
       credentials: true,
     }),
   )
 
-  // Usar variable de entorno PORT para Render
   await app.listen(process.env.PORT || 3000)
 }
 bootstrap()

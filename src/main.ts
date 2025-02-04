b580b9a057299e43ead17b5dd69e244e9fda1a2d
maximilianopontin <maxipontin92@gmail.com>
2025-02-03 21:37:26 -0300

    f

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,24 @@
-
-import { AppModule } from './app.module';
-import { NestFactory } from '@nestjs/core';
-import { NextFunction, Request, Response } from 'express';
-import { ValidationPipe } from '@nestjs/common';
-import * as cors from 'cors';
+import { NestFactory } from "@nestjs/core"
+import { AppModule } from "./app.module"
+import { ValidationPipe } from "@nestjs/common"
+import * as cors from "cors"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true
-  }))
+  const app = await NestFactory.create(AppModule)
 
-  app.enableCors({
-    origin: 'https://api-react-movie.onrender.com', 
-    // CORS HTTP methods
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  });
-  app.use(cors());
+  app.useGlobalPipes(new ValidationPipe()) // Moved this line up
 
-  await app.listen(3000);
+  // Configurar CORS
+  app.use(
+    cors({
+      origin: ["https://api-react-movie.onrender.com", "http://localhost:5173"],
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      credentials: true,
+    }),
+  )
+
+  // Usar variable de entorno PORT para Render
+  await app.listen(process.env.PORT || 3000)
 }
+bootstrap()
 
-bootstrap();

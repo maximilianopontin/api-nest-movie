61039fb61ae3e1112881e258c518efde48ae3ffc
maximilianopontin <maxipontin92@gmail.com>
2025-02-03 22:41:24 -0300

    f

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const PORT = process.env.PORT || 8080;
   app.enableCors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
@@ -11,6 +11,6 @@ async function bootstrap() {
     credentials: true, // Si usas cookies o autenticación
   });
 
-  await app.listen(3000);
+  await app.listen(8080);
 }
 bootstrap();

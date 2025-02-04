176ab7d841e512207c4daa359ac88018e74f2352
maximilianopontin <maxipontin92@gmail.com>
2025-02-04 18:04:16 -0300

    f

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  app.use(cors()); // Habilitar CORS
- // app.use(helmet()); 
-  await app.listen(8080);//cambio a puerto 8080 para desarrollo
+
+  // Configuración de CORS
+  app.enableCors({
+    origin: '*', // Elimina el slash final si existe
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    credentials: true, // Permite el uso de cookies si es necesario
+  });
+
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
